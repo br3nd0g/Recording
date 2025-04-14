7b2234f984a697fbc15233758a08b610fce8633a
br3nd0g <brendan.j.corcoran@gmail.com>
2025-04-14 12:30:20 +0200

    everything initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Recording
+
+Recording is a game that explores the ethical dilemma of Live Facial Recognition used by the police, through the lens of a preference utilitarian framework.
---
+++ index.js
@@ -0,0 +1,43 @@
+function whiteFlash() {
+    const whiteFlash = document.getElementById('whiteFlash');
+    if (whiteFlash) {
+        whiteFlash.style.display = 'inline-block';
+        whiteFlash.style.opacity = '1';
+        audio.play();
+    }
+    setTimeout(() => {
+        if (whiteFlash) {
+            whiteFlash.style.opacity = '0';
+        }
+
+        setTimeout(() => {
+            if (whiteFlash) {
+                whiteFlash.remove();
+            }
+
+            let cur_url = window.location.href
+            let cur_url_sub = cur_url.substring(0, cur_url.search('index.html'));
+
+            window.location.href = cur_url_sub + 'game.html';
+        }, 100); 
+    }, 100);
+}
+
+const startButton = document.getElementById('startButton');
+const cameraOverlay = document.getElementById('cameraOverlay');
+
+var audio = new Audio('./static/audio/shutter.mp3');
+
+if (startButton && cameraOverlay) {
+    startButton.addEventListener('mouseover', () => {
+        cameraOverlay.style.display = 'inline-block';
+        cameraOverlay.style.opacity = '1';
+    });
+
+    startButton.addEventListener('mouseout', () => {
+        cameraOverlay.style.opacity = '0';
+        setTimeout(() => {
+            cameraOverlay.style.display = 'none';
+        }, 100); // Delay to allow opacity transition
+    });
+}
